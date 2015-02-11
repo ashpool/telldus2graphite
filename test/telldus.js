b07dbe322375371c6c5eb9d2dfa63d07020c1533
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-02-11 07:50:34 +0100

    Add test for getSensors failure

--- test/telldus.js
+++ test/telldus.js
@@ -1,53 +1,73 @@
-var RSVP = require('rsvp');
-var assert = require("assert")
+/*jshint undef:false */
+var assert = require('assert');
 
 describe('telldus', function () {
-	describe('#getSensors()', function () {
-		it('should return an array of sensors', function (done) {
-			var sensors = {
-					"sensor": [{
-						"id": "2813567",
-						"name": "firstfloor",
-						"lastUpdated": 1423582025,
-						"ignored": 0,
-						"client": "58229",
-						"clientName": "Hemma",
-						"online": "1",
-						"editable": 1,
-						"battery": 254
-					}, {
-						"id": "2814769",
-						"name": "freezer",
-						"lastUpdated": 1423582285,
-						"ignored": 0,
-						"client": "58229",
-						"clientName": "Hemma",
-						"online": "1",
-						"editable": 1,
-						"battery": 254
-					}, {
-						"id": "2813556",
-						"name": "fridge",
-						"lastUpdated": 1423582490,
-						"ignored": 0,
-						"client": "58229",
-						"clientName": "Hemma",
-						"online": "1",
-						"editable": 1,
-						"battery": 254
-					}]
-				},
-				telldusAPI = {
-					getSensors: function (cb) {
-						return cb(null, sensors);
-					}
-				};
+	var sensors = {};
 
+	beforeEach(function () {
+		sensors = {
+			sensor: [{
+				id: '2813567',
+				name: 'firstfloor',
+				lastUpdated: 1423582025,
+				ignored: 0,
+				client: '58229',
+				clientName: 'Hemma',
+				online: '1',
+				editable: 1,
+				battery: 254
+			}, {
+				id: '2814769',
+				name: 'freezer',
+				lastUpdated: 1423582285,
+				ignored: 0,
+				client: '58229',
+				clientName: 'Hemma',
+				online: '1',
+				editable: 1,
+				battery: 254
+			}, {
+				id: '2813556',
+				name: 'fridge',
+				lastUpdated: 1423582490,
+				ignored: 0,
+				client: '58229',
+				clientName: 'Hemma',
+				online: '1',
+				editable: 1,
+				battery: 254
+			}]
+		};
+	});
+
+	describe('#getSensors()', function () {
+		it('returns an array of sensors - if everything works fine', function (done) {
+			var telldusAPI = {
+				getSensors: function (cb) {
+					return cb(null, sensors);
+				}
+			};
 			var telldus = require('../lib/telldus')(telldusAPI);
 			telldus.getSensors().then(function (value) {
 				assert.equal(value, sensors);
 				done();
 			});
 		});
-	})
+		it('handles failure - if the API fails', function (done) {
+			var telldusAPI = {
+				getSensors: function (cb) {
+					var err = {};
+					err.message = 'failure';
+					return cb(err, sensors);
+				}
+			};
+			var telldus = require('../lib/telldus')(telldusAPI);
+			telldus.getSensors().then(function (value) {
+				assert.fail(value);
+			}).catch(function (rejection) {
+				assert.equal(rejection, 'failure');
+				done();
+			});
+		});
+	});
 });
