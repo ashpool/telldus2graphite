2194a8acf80e0b3be12a6907009811a5fd645610
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-02-19 15:02:50 +0100

    Refactor

--- bin/telldus2graphite.js
+++ bin/telldus2graphite.js
@@ -5,6 +5,6 @@ var home = require('./../lib/homepath'),
 	config = require(home.path() + '/.telldus2graphite/config.json'),
 	graphite = require('./../lib/graphite.js')(config),
 	telldusClient = require('telldus-live-promise').Client(config),
-	telldus = require('telldus-live-promise').Promise(telldusClient, config);
+	telldus = require('telldus-live-promise').Telldus(telldusClient, config);
 
 telldus.getSensors().then(telldus.getSensorInfos).then(graphite.logAll);
