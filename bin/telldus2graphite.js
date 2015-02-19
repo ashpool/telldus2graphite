4b21d4e67cd464472cc1461653113a4c2967cbc6
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-02-19 13:33:33 +0100

    Using telldus-live-promise

--- bin/telldus2graphite.js
+++ bin/telldus2graphite.js
@@ -4,7 +4,7 @@
 var home = require('./../lib/homepath'),
 	config = require(home.path() + '/.telldus2graphite/config.json'),
 	graphite = require('./../lib/graphite.js')(config),
-	telldusClient = require('telldus-live').Client(config),
-	telldus = require('telldus-live').Promise(telldusClient, config);
+	telldusClient = require('telldus-live-promise').Client(config),
+	telldus = require('telldus-live-promise').Promise(telldusClient, config);
 
 telldus.getSensors().then(telldus.getSensorInfos).then(graphite.logAll);
