6b4e93cefaeb75f8dece61ccef935a54eee93d5e
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-02-19 09:07:52 +0100

    Remove cruft

--- bin/telldus2graphite.js
+++ bin/telldus2graphite.js
@@ -1,16 +1,10 @@
 /*
  Typically invoked by cron.
  */
-var RSVP = require('rsvp'),
-	home = require('./../lib/homepath'),
+var home = require('./../lib/homepath'),
 	config = require(home.path() + '/.telldus2graphite/config.json'),
 	graphite = require('./../lib/graphite.js')(config),
-	logger = require('log4js-extras')(config).getLogger(__filename),
 	telldusAPI = require('./../lib/telldusAPI').API(config),
 	telldus = require('./../lib/telldus')(telldusAPI, config);
-
-RSVP.on('error', function (reason) {
-	logger.debug(reason);
-});
 
 telldus.getSensors().then(telldus.getSensorInfos).then(graphite.logAll);
