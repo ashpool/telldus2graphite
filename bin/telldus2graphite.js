8a8d096f3462d918fb7a1d0c7bd4649fb73d8865
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-02-19 09:04:56 +0100

    Extract telldusAPI module

--- bin/telldus2graphite.js
+++ bin/telldus2graphite.js
@@ -2,28 +2,15 @@
  Typically invoked by cron.
  */
 var RSVP = require('rsvp'),
-	TelldusAPI = require('telldus-live'),
 	home = require('./../lib/homepath'),
 	config = require(home.path() + '/.telldus2graphite/config.json'),
 	graphite = require('./../lib/graphite.js')(config),
 	logger = require('log4js-extras')(config).getLogger(__filename),
-	start = new Date().getTime();
+	telldusAPI = require('./../lib/telldusAPI').API(config),
+	telldus = require('./../lib/telldus')(telldusAPI, config);
 
 RSVP.on('error', function (reason) {
 	logger.debug(reason);
 });
-
-var telldusAPI = new TelldusAPI.TelldusAPI({
-	publicKey: config.telldusPublicKey,
-	privateKey: config.telldusPrivateKey
-}).login(config.telldusToken, config.telldusTokenSecret, function (err, user) {
-		if (!!err) {
-			return logger.error('login error: ', err.data);
-		}
-		logger.info('user: ', user);
-	}).on('error', function (err) {
-		logger.error('background error: ', err.message);
-	}),
-	telldus = require('./../lib/telldus.js')(telldusAPI, config);
 
 telldus.getSensors().then(telldus.getSensorInfos).then(graphite.logAll);
