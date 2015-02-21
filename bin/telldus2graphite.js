50353cfb1b83379f17637d982e08af45c13c1301
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-02-21 13:43:22 +0100

    Update README

--- bin/telldus2graphite.js
+++ bin/telldus2graphite.js
@@ -4,8 +4,10 @@
 var home = require('./../lib/homepath'),
 	config = require(home.path() + '/.telldus2graphite/config.json'),
 	graphite = require('./../lib/graphite.js')(config),
-	client = require('telldus-live-promise').Client(config),
-	sensors = require('telldus-live-promise').Sensors(client),
 	logger = require('log4js-extras')(config).getLogger(__filename);
+config.logger = logger;
+var telldus = require('telldus-live-promise'),
+	client = telldus.Client(config),
+	sensors = telldus.Sensors(client);
 
 sensors.getSensors().then(sensors.getSensorInfos).then(graphite.logAll).catch(logger.error);
