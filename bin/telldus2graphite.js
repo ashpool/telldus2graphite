268d8ddf3477e295317e5eb0b9b3adc25a681d67
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-02-26 19:46:19 +0100

    Up dependency telldus-live-promise to v0.1.0

--- bin/telldus2graphite.js
+++ bin/telldus2graphite.js
@@ -2,12 +2,12 @@
  Typically invoked by cron.
  */
 var home = require('./../lib/homepath'),
-	config = require(home.path() + '/.telldus2graphite/config.json'),
-	graphite = require('./../lib/graphite.js')(config),
-	logger = require('log4js-extras')(config).getLogger(__filename);
+    config = require(home.path() + '/.telldus2graphite/config.json'),
+    graphite = require('./../lib/graphite.js')(config),
+    logger = require('log4js-extras')(config).getLogger(__filename);
 config.logger = logger;
 var telldus = require('telldus-live-promise'),
-	client = telldus.Client(config),
-	sensors = telldus.Sensors(client);
+    api = telldus.API(config),
+    sensors = telldus.Sensors(api);
 
 sensors.getSensors().then(sensors.getSensorInfos).then(graphite.logAll).catch(logger.error);
