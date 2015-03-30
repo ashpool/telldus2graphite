e4284b84834aed30c20ef6a5a6c2191c3c75c90d
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-03-30 11:00:08 +0200

    Change isTimestamp definition

--- test/validator.js
+++ test/validator.js
@@ -6,16 +6,6 @@ chai.should();
 describe('validator', function () {
     var validator = require('../lib/validator');
     describe('#isNumber', function () {
-        it('null is not a number', function () {
-            validator.isNumber(null).should.equal(false);
-        });
-        it('a string is not a number', function () {
-            validator.isNumber('a string').should.equal(false);
-        });
-        it('undefined is not a number', function () {
-            var undef;
-            validator.isNumber(undef).should.equal(false);
-        });
         it('42 is a number', function () {
             validator.isNumber(42).should.equal(true);
         });
@@ -28,8 +18,24 @@ describe('validator', function () {
         it('"3.14" is a number', function () {
             validator.isNumber('3.14').should.equal(true);
         });
+        it('null is not a number', function () {
+            validator.isNumber(null).should.equal(false);
+        });
+        it('a string is not a number', function () {
+            validator.isNumber('a string').should.equal(false);
+        });
+        it('undefined is not a number', function () {
+            var undef;
+            validator.isNumber(undef).should.equal(false);
+        });
     });
     describe('#isTimestamp', function () {
+        it('1000000000001 is a timestamp', function () {
+            validator.isTimestamp(1200000000001).should.equal(true);
+        });
+        it('new Date().getTime() is a timestamp', function () {
+            validator.isTimestamp(new Date().getTime()).should.equal(true);
+        });
         it('null is not a timestamp', function () {
             validator.isTimestamp(null).should.equal(false);
         });
@@ -40,11 +46,8 @@ describe('validator', function () {
             var undef;
             validator.isTimestamp(undef).should.equal(false);
         });
-        it('1000000000000 is not a timestamp', function () {
+        it('999999999999 is not a timestamp', function () {
             validator.isTimestamp(1000000000000).should.equal(false);
-        });
-        it('1200000000001 is a timestamp', function () {
-            validator.isTimestamp(1200000000001).should.equal(true);
         });
     });
 });
