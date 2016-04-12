55069ae61feb27528e375823a48bc33af615c5fe
Magnus Ljadas <magnus.ljadas@gmail.com>
2016-04-12 17:46:08 +0200

    Upgrade and eliminate deps

--- bin/telldus2graphite.js
+++ bin/telldus2graphite.js
@@ -2,12 +2,22 @@
  Typically invoked by cron.
  */
 var home = require('./../lib/homepath'),
-    config = require(home.path() + '/.telldus2graphite/config.json'),
-    graphite = require('./../lib/graphite.js')(config),
-    logger = require('./../lib/logger')(config).getLogger(__filename);
+  config = require(home.path() + '/.telldus2graphite/config.json'),
+  graphite = require('./../lib/graphite.js')(config),
+  logger = require('./../lib/logger')(config).getLogger(__filename);
 config.logger = logger;
 var telldus = require('telldus-live-promise'),
-    api = telldus.API(config),
-    sensors = telldus.Sensors(api);
+  api = telldus.API(config),
+  sensors = telldus.Sensors(api);
 
-sensors.getSensors().then(sensors.getSensorInfos).then(graphite.logAll).catch(logger.error);
+function read(list) {
+  return list.sensor.filter(function(sensor) {
+    return sensor.id !== undefined;
+  }).map(sensors.info);
+}
+
+function log(list) {
+  Promise.all(list).then(graphite.logAll).catch(logger.error);
+}
+
+sensors.list().then(read).then(log);
