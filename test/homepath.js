76cb55fd5106e634a3892747711ae913365da25c
Magnus Ljadas <magnus.ljadas@gmail.com>
2016-04-15 21:02:04 +0200

    Introduce .editorconfig
    Stolen from https://github.com/facebook/react/blob/master/.editorconfig

--- test/homepath.js
+++ test/homepath.js
@@ -2,11 +2,11 @@
 var chai = require('chai');
 chai.should();
 
-describe('homepath', function () {
-    describe('#path', function () {
-        it('output vary depending on OS', function () {
-            var home = require('../lib/homepath');
-            home.path().should.equal(process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE);
-        });
+describe('homepath', function() {
+  describe('#path', function() {
+    it('output vary depending on OS', function() {
+      var home = require('../lib/homepath');
+      home.path().should.equal(process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE);
     });
+  });
 });
