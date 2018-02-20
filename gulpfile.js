32dde7f1e4e07cb653f023ef46ebf917808b08b0
Magnus Ljadas <magnus.ljadas@gmail.com>
2018-02-20 14:28:34 +0100

    :hocho: deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,12 @@
 var gulp = require('gulp'),
-  jshint = require('gulp-jshint'),
-  mocha = require('gulp-mocha'),
-  cover = require('gulp-coverage');
+  mocha = require('gulp-mocha');
 
-gulp.task('default', ['lint', 'test'], function() {
+gulp.task('default', ['test'], function() {
   // place code for your default task here
-});
-
-gulp.task('lint', function() {
-  return gulp.src(['./lib/*.js', './bin/*.js', './test/*.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter('default', { verbose: true }));
 });
 
 gulp.task('test', function() {
   return gulp.src('./test', { read: false })
-    .pipe(cover.instrument({
-      pattern: ['*lib/*.js'],
-      debugDirectory: 'debug'
-    }))
     .pipe(mocha({ reporter: 'nyan' }))
-    .pipe(cover.gather())
-    .pipe(cover.format())
     .pipe(gulp.dest('reports'));
 });
