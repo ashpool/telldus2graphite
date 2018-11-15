237d441d4dcbdbe88ab9f29dc9ee800bfc9dadbe
Magnus Ljadas <magnus.ljadas@gmail.com>
2018-11-15 07:56:16 +0100

    Typsescriptify

--- lib/graphite.ts
+++ lib/graphite.ts
@@ -1,19 +1,16 @@
-var {GraphiteClient} = require('graphite-promise'),
-  validator = require('./validator');
+// @ts-ignore
+import {GraphiteClient} from 'graphite-promise';
+import validator from './validator';
 
-/*
- Anatomy of a sensorInfo:
- */
-
-module.exports = function(config) {
+module.exports = function(config: Record<string, string>) {
   const metric = require('./metric')(config.format);
   const client = config.client || new GraphiteClient(config);
 
-  function logMetric(metric, timestamp) {
+  function logMetric(metric: Record<string, any>, timestamp: number) {
     return client.write(metric, timestamp);
   }
 
-  function _logSensorInfo(sensorInfo) {
+  function _logSensorInfo(sensorInfo: Record<string, any>) {
     return new Promise(function(resolve, reject) {
       if (!sensorInfo) {
         return reject(new Error('sensorInfo must not be empty'));
@@ -24,7 +21,8 @@ module.exports = function(config) {
       if (!validator.isTimestamp(sensorInfo.lastUpdated)) {
         return reject(new Error('lastUpdated is not a timestamp'));
       }
-      const promises = sensorInfo.data.map(function(data) {
+      // @ts-ignore
+      const promises = sensorInfo.data.map(data => {
         if (!data.name) {
           return reject(new Error('data must have a name'));
         }
@@ -35,9 +33,9 @@ module.exports = function(config) {
           ts = (sensorInfo.lastUpdated * 1000) + sensorInfo.timezoneoffset || 0;
         return logMetric(m, ts);
       });
-      return Promise.all(promises).then(function(metric) {
+      return Promise.all(promises).then((metric) => {
         resolve(metric);
-      }, function(error) {
+      }, (error: Error) => {
         reject(error);
       });
     });
@@ -47,7 +45,7 @@ module.exports = function(config) {
    * Log all sensor infos
    * @param sensorInfos list of sensor infos
    */
-  function logAll(sensorInfos) {
+  function logAll(sensorInfos: Record<string, any>[]) {
     return Promise.all(sensorInfos.filter( s => s.name).map(_logSensorInfo));
   }
 
