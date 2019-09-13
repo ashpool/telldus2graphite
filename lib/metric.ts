a35e837e69c515bdfc634f6529f90ed2345cc09c
Magnus Ljadas <magnus.ljadas@gmail.com>
2019-09-13 10:28:36 +0200

    Add github workflow

--- lib/metric.ts
+++ lib/metric.ts
@@ -1,18 +1,18 @@
-module.exports = function(formatString) {
-  var formatArray = (formatString || 'client.sensor.type').split('.');
+module.exports = function(formatString: string) {
+  const formatArray: string[] = (formatString || 'client.sensor.type').split('.');
 
-  function create(sensorInfo, data) {
-    var translaton = {
+  function create(sensorInfo: Record<string, string | number>, data: Record<string, string | number>) {
+    const translation: Record<string, string> = {
         client: sensorInfo.clientName,
         sensor: sensorInfo.name,
         type: data.name
-      },
-      properties = formatArray.slice(0),
-      metric = {};
-    metric[translaton[properties.pop()]] = Number(data.value);
+      };
+      const properties: string[] = formatArray.slice(0);
+      let metric: Record<string, string | number> = {};
+    metric[translation[properties.pop()]] = Number(data.value);
     while (properties.length > 0) {
-      var temp = {};
-      temp[translaton[properties.pop()]] = metric;
+      let temp: Record<string, string | number> = {};
+      temp[translation[properties.pop()]] = metric;
       metric = temp;
     }
     return metric;
